16cad1b2de8f9e2e813b9fc15bfa2fb5ca6b1802
Tomasz Majerowicz <151870534+t0m-m@users.noreply.github.com>
2026-01-15 00:53:02 +0100

    Implement button state tracking and scanning
    Added button state detection functionality.

--- Software/buttons.c
+++ Software/buttons.c
@@ -7,10 +7,12 @@
 
 //inicjalizacja przyciskow
 void init_buttons(){
+
     //row inits
     gpio_init(ROW_0);
     gpio_init(ROW_1);
     gpio_init(ROW_2);
+
     //ustawiamy rowy jako outputy, poniewaz bedziemy skanowac matryce po linijkach, jedna na raz
     gpio_set_dir(ROW_0, GPIO_OUT); 
     gpio_set_dir(ROW_1, GPIO_OUT);
@@ -41,22 +43,36 @@ void init_buttons(){
 
 //funkcja do skanowania matrycy
 void matrix_scan(int row_table[3], int column_table[3], int buttons_pressed[3][3]){
-    for(int x = 0; x <= 2; x++){
+
+    for(int x = 0; x < 3; x++){
+
         //zerujemy jeden wiersz na raz
         gpio_put(row_table[x], 0);
 
-        for(int y = 0; y <=2; y++){
+        for(int y = 0; y < 3; y++){
             if(gpio_get(column_table[y]) == 0){
-
                 buttons_pressed[x][y] = 1;
-
             }
             else{
-
                 buttons_pressed[x][y] = 0;
-
             }
         }
         gpio_put(row_table[x], 1);
+    }
+};
+
+//funkcja do zczytywania przyciskow
+void button_states(int buttons_pressed_prev[3][3], int buttons_pressed[3][3]){
+    
+    for(int x = 0; x < 3; x++){
+        for(int y = 0; y < 3; y++){
+            if(buttons_pressed_prev[x][y] == 0 && buttons_pressed[x][y] == 1){ //nastapila zmiana SW0 z 0 na 1, czyli przycisk zostal wcisniety
+                // tutaj chce wyslac taki przycisk jaki bedzie wcisniety
+            }
+            else if(buttons_pressed_prev[x][y] == 1 && buttons_pressed[x][y] == 0){ //nastapila zmiana SW0 z 1 na 0, czyli przycisk zostal wycisniety
+                //mozliwa jakas akcja na wycisniecie
+            }
+            buttons_pressed_prev[x][y] = buttons_pressed[x][y]; // aktualizacja stanu poprzedniego
+        }
     }
 };
